cba3591fea9053275898e26e853f2df1fd903a36
Nicolas <nicolas@rvola.com>
2020-08-24 16:09:23 +0200

    ✔︎ Compatibility WOO 4.4

--- woo-cancel-abandoned-order.php
+++ woo-cancel-abandoned-order.php
@@ -19,7 +19,7 @@ Requires at least:      4.0
 Tested up to:           5.4
 Requires PHP:           7.0
 WC requires at least:   2.2
-WC tested up to:        4.0
+WC tested up to:        4.4
 
 License:                GNU General Public License v3.0
 License URI:            https://www.gnu.org/licenses/gpl-3.0.html
